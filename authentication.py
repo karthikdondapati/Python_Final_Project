234ae08b1a3b3204c98d1dc0322ad63256c4da37
karthik <karthik.dondapati@gmail.com>
2018-06-25 17:14:55 -0700

    Changes Final
    Changes Final

---
+++ authentication.py
@@ -0,0 +1,16 @@
+class authentication:
+
+	def __init__(self):
+		self.consumer_key="K2WHQjH2yCbAWQKXY8IUAQd5T"
+		self.consumer_secret="7RvSNposUestR34zV4rAhlKLtml1tlux9ZhGSp3WomIf2Kvpbi"
+		self.access_token="2259461466-81b4w4XsR9KnvQHulWxPWswbfMMbe8CG2CbtR0U"
+		self.access_token_secret="7G8KwAbt2kPoQneG3I4kLMBMTJ8MBftXRCLG1NaQ1MlYK"
+
+	def getconsumer_key(self):
+		return self.consumer_key
+	def getconsumer_secret(self):
+		return self.consumer_secret
+	def getaccess_token(self):
+		return self.access_token
+	def getaccess_token_secret(self):
+		return self.access_token_secret
